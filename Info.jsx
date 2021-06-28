8fe050e7aac61ffb277845a03d39b1952261f898
meena939 <86585843+meena939@users.noreply.github.com>
2021-06-28 10:47:19 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1 align="center">Tableau Fundamentals</h1>
+      <p>
+        Here we bring free 7 days live BOOTCAMP with a hands-on project at the
+        end - Tableau Fundamentals bootcamp.
+      </p>
+    </div>
+  );
+}
+export default Info;
